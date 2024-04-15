ee1178fe96fa29c4f3f9cc2ac176bc13b8672977
unknown <ketandograch22@gmail.com>
2024-04-15 09:28:02 +0530

    added dict to store attendance

--- FaceDetect.py
+++ FaceDetect.py
@@ -28,6 +28,8 @@ class Video():
         self.enrollment = self.FaceData[: , -1]
         self.model = KNeighborsClassifier()
         self.model.fit(self.face ,self.enrollment)
+        self.presentStudents = dict()
+        self.counter = 0
         
     def capture(self,Class):
         detector = get_frontal_face_detector()
@@ -53,14 +55,25 @@ class Video():
                     GrayFace = np.array(GrayFace).flatten()
                     
                     #predicting face
-                    name = self.PredictFace([GrayFace])
-                    self.TakeAttendance(Class , name)
+                    enrollment = self.PredictFace([GrayFace])
+                    
+                    if enrollment != "not recognized":
+                        cv.putText(frame,enrollment,(x1,y1),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+                        if enrollment[0] not in self.presentStudents:
+                            self.presentStudents[self.counter] = enrollment
+                    else:
+                        cv.putText(frame,enrollment,(x1,y1),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+                    
+                else:
+                    cv.putText(frame,"No face detected",(50,50),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+                    
                 cv.imshow("frame",frame)
                 
             key = cv.waitKey(100)
             if key == ord('q'):
                 cap.release()
                 cv.destroyAllWindows()
+                self.TakeAttendance(Class , enrollment)
                 break
         
     def GetClass(self):
@@ -76,11 +89,16 @@ class Video():
         button.place(x=160 , y=300 , width=200 , height=50)
 
     def PredictFace(self,face):
+        distances, indices = self.model.kneighbors(face)
+        if distances[0][0] > 10000:
+            return "not recognized"
         return self.model.predict(face)[0]
+        
 
-    def TakeAttendance(self, Class,name):
-        print(Class,name)
+    def TakeAttendance(self, Class,enrollment):
+        print(self.presentStudents) 
+        
         #sql and shit goes here
 
-# vid = Video()
-# vid.capture("ML")
+vid = Video()
+vid.capture("ML")
