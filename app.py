04c542525f2bc9bb414ec86122c9291a17d6a66e
unknown <ketandograch22@gmail.com>
2024-04-20 04:37:45 +0530

    added some background images

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import FaceDetect
 import GatherData
 import numpy as np
 import pandas as pd
+from PIL import Image, ImageTk
 
 #creating instance of FaceDetect class
 FaceDetectInstance = FaceDetect.Video()
@@ -21,63 +22,68 @@ except (Exception, psql.DatabaseError) as error:
 psqlcur = psqlcon.cursor()
 
 class App():
-    def MainWindow(self):
+    def __init__(self):
         #setting up main screen
-        MainScreen = Tk()
-        MainScreen.geometry("500x600")
-        MainScreen.resizable(0,0)
-        MainScreen.title("Attendance System")
+        self.MainScreen = Tk()
+        self.MainScreen.geometry("500x600")
+        self.MainScreen.resizable(0,0)
+        self.MainScreen.title("Attendance System")
         
-        canvas = Canvas(MainScreen , background="blue")
-        canvas.place(x=0 , y=0, width=500 , height=600)
+        self.canva= Canvas(self.MainScreen)
+        self.canva.place(x=0,y=0,width=500,height=600)
+        self.img= ImageTk.PhotoImage(Image.open("images\\mainbg.png"))
+        self.canva.create_image(0,0,anchor=NW,image=self.img)
 
         #creating buttons
-        CaptureButton = Button(MainScreen , text="Capture" ,command = lambda: (self.CaptureWindow()))
-        DataButton = Button(MainScreen , text="Add Students", command=lambda:(self.DataWindow() , MainScreen.destroy()))
-        AddClassButton = Button(MainScreen , text="Add Class", command=lambda: (self.AddClassWindow() , MainScreen.destroy()))
-        SeeAttendanceButton = Button(MainScreen , text="See Attendance", command=lambda: (self.SeeAttendance() , MainScreen.destroy()))
+        self.TakeAttendanceButton = Button(self.MainScreen , text="TAKE\nATTENDANCE" ,command = lambda: (self.GetClass()) , bg = "#FFFFFF" , activebackground="#f0f0ff" , fg="#524646" , font=("Arial", 12 , "bold"))
+        self.DataButton = Button(self.MainScreen , text="ADD\nSTUDENTS", command=lambda:(GatherDataInstance.DataWindow()) , bg = "#FFFFFF", activebackground="#ffeef8" , fg="#524646", font=("Arial", 12 , "bold"))
+        self.AddClassButton = Button(self.MainScreen , text="ADD\nCLASS", command=lambda: (self.AddClassWindow() ) , bg = "#FFFFFF", activebackground="#ffeef8" , fg="#524646", font=("Arial", 12 , "bold"))
+        self.SeeAttendanceButton = Button(self.MainScreen , text="SEE\nATTENDANCE", command=lambda: (self.SeeAttendance() ) , bg = "#FFFFFF", activebackground="#f0f0ff" , fg="#524646", font=("Arial", 12 , "bold") )
 
         #placing buttons
-        CaptureButton.place(x=160, y=70 , height=50 , width=200)
-        DataButton.place(x=160, y=200 , height=50 , width=200)
-        AddClassButton.place(x=160, y=330 , height=50 , width=200)
-        SeeAttendanceButton.place(x=160, y=460 , height=50 , width=200)
+        self.TakeAttendanceButton.place(x=77, y=150 , height=136 , width=164)
+        self.DataButton.place(x=288, y=150 , height=136 , width=164)
+        self.AddClassButton.place(x=77, y=339 , height=136 , width=164)
+        self.SeeAttendanceButton.place(x=288, y=340 , height=136 , width=164)
 
-    def CaptureWindow(self):
-        #setting up capture screen
-        CaptureScreen = Tk()
-        CaptureScreen.geometry("500x500")
-        CaptureScreen.resizable(0,0)
-        CaptureScreen.title("Capture")
+    def GetClass(self):
+        self.MainScreen.withdraw()
+        screen = Toplevel()
+        screen.geometry("500x600")
+        screen.resizable(0,0)
+        screen.title("Class")
+
+        try:
+            self.img = PhotoImage(file="images\\takeattendancebg.png")
+            label_bgImage = Label(screen,image=self.img)
+            label_bgImage.place(x=0, y=0)
+        except Exception as e:
+            print(e)
+
+        psqlcur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';")
+        classes = psqlcur.fetchall()
+        classes = [x[0] for x in classes]
+
+        #creating menu
         
-        canvas = Canvas(CaptureScreen , background="red")
-        canvas.place(x=0 , y=0, width=500 , height=600)
+        ClassOptions=StringVar(screen )
+        ClassOptions.set("SELECT CLASS")
+        Class=OptionMenu(screen,ClassOptions,*classes)
+        Class.place(x=104,y=253,height=97,width=326)
 
-        #creating buttons
-        CaptureButton = Button(CaptureScreen , text="Capture" , command=lambda:(FaceDetectInstance.GetClass(),CaptureScreen.destroy()))
-        BackButton = Button(CaptureScreen , text="<-" , command=lambda: (self.MainWindow(),CaptureScreen.destroy()))
+        Class.config(
+            bg="#ffffff",  # background color
+            fg="black",  # text color
+            font=("Arial", 14 , "bold"),  # font and font size
+            bd = -1,
+            activebackground="#ffffff",  # background color when clicked
+            highlightbackground="#ffffff",  # background color when focused
+        )
 
-        #placing buttons
-        CaptureButton.place(x=160, y=150 , height=50 , width=200)
-        BackButton.place(x=10, y=10 , height=30 , width=40)
 
-    def DataWindow(self):
-        #setting up add data screen
-        DataScreen = Tk()
-        DataScreen.geometry("500x500")
-        DataScreen.resizable(0,0)
-        DataScreen.title("Add Data")
-        
-        canvas = Canvas(DataScreen , background="green")
-        canvas.place(x=0 , y=0, width=500 , height=600)
+        button = Button(screen , text="Submit" , command=lambda: (FaceDetectInstance.capture(ClassOptions.get())) , bg="#dff5f6" , activebackground="#dff5f6" , fg="#524646" , font=("Arial", 12 , "bold") , borderwidth=0)
+        button.place(x=197 , y=418 , width=118 , height=120)
 
-        #creating buttons
-        AddButton = Button(DataScreen , text="Add" , command=lambda: GatherDataInstance.GatherData())
-        BackButton = Button(DataScreen , text="<-" , command=lambda: (self.MainWindow(),DataScreen.destroy()))
-
-        #placing buttons
-        AddButton.place(x=160, y=150 , height=50 , width=200)
-        BackButton.place(x=10, y=10 , height=30 , width=40)
 
     def AddClassWindow(self ):
         #setting up Add Class screen
@@ -88,10 +94,10 @@ class App():
         entry = Entry(ClassScreen)
 
         #adding buttons
-        BackButton = Button(ClassScreen , text="<-" , command=lambda: (self.MainWindow(),ClassScreen.destroy()))
+
         AddButoon = Button(ClassScreen , text="ADD" , command = lambda:(psqlcur.execute("CREATE TABLE {}(enrollment VARCHAR(11), name VARCHAR(30) , day DATE , present BOOLEAN);".format(entry.get())) , psqlcon.commit()))
         #placing buttons
-        BackButton.place(x=10, y=10 , height=30 , width=40)
+
         AddButoon.place(x=160, y=150 , height=50 , width=200)
         entry.place(x=160 , y = 120 , height = 50 , width = 200)
         ## This function will have multiple lables
@@ -124,9 +130,6 @@ class App():
 
         GetTotalAttendance = Button(screen , text = "total attendance" , command = lambda: self.GetTotalAttendance(ClassOptions.get()))
         GetTotalAttendance.place(x=360 , y=400 , width=200 , height=50)
-
-        BackButton = Button(screen , text="<-" , command=lambda: (self.MainWindow(),screen.destroy()))
-        BackButton.place(x=10, y=10 , height=30 , width=40)
 
     def ShowAttendance(self,enrollment,Class ,screen):
         try:
@@ -180,5 +183,4 @@ class App():
 
 #vid = FaceDetect.video.capture()
 App = App()
-App.MainWindow()
 mainloop()
